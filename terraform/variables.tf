c5964b0050c6f4181bc52a996fac0aa0d8c63140
Ayush Saroha <ayushsaroha999@gmail.com>
2026-05-25 15:54:28 +0530

    fixed deployment refresh

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
-  default     = "ap-south-1"
+  default     = "ap-east-2"
 }
 
 variable "project_name" {
